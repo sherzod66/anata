7bfb61f55f9fcb16affa1b0cee48a943b2910106
Sherzod <96307270+sherzod66@users.noreply.github.com>
2023-07-09 11:22:34 +0500

    Delete src directory

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './components/global.css'
-import Router from './components/Router.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router />
-)
