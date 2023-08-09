17eb259ab8fe63845ce2fcbdd9385719f7920874
Sherzod <96307270+sherzod66@users.noreply.github.com>
2023-08-09 12:42:35 +0500

    Delete src directory

--- src/components/ui/ErrorFileds.jsx
+++
@@ -1,7 +0,0 @@
-import './ui.css'
-const Errorfilled = () => {
-    return <div id='error-filled' className="error">
-        <div className='error__item'>Вы не заполнили все поля</div>
-    </div>
-}
-export default Errorfilled
--- src/components/ui/LoadedInfo.jsx
+++
@@ -1,7 +0,0 @@
-import './ui.css'
-const LoadedInfo = () => {
-    return <div id='loaded-info' className="loaded">
-        <div className='error__item'>Данные успешно добавленны</div>
-    </div>
-}
-export default LoadedInfo
