4ac65d82a1b9eda1c43a8058cc8183a24ea67812
Sherzod <96307270+sherzod66@users.noreply.github.com>
2023-08-10 07:45:42 +0300

    Delete src directory

--- src/components/ui/ErrorUpload.jsx
+++
@@ -1,7 +0,0 @@
-import './ui.css'
-const ErrorUpload = () => {
-    return <div id='error-upload' className="error">
-        <div className='error__item'>Что-то пошло не так</div>
-    </div>
-}
-export default ErrorUpload
