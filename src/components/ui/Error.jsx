15f962deb9c4584ee2a0dfcdb2b16058ebdf68a5
Sherzod <96307270+sherzod66@users.noreply.github.com>
2023-08-19 13:28:49 +0500

    Delete src directory

--- src/components/ui/Error.jsx
+++
@@ -1,8 +0,0 @@
-import './ui.css'
-const ErrorPopap = () => {
-    return <div id='error-window' className="error">
-        <div className='error__item'>Вы не загрузили фото</div>
-    </div>
-}
-export default ErrorPopap
-
